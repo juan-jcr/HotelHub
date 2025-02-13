3e492ae99114389c15bf54ca7e24fd24c98d0288
Juan Antonio Castañeda <castaneda2850@gmail.com>
2025-02-13 13:07:40 -0500

    initial commit

---
+++ HotelHub/src/test/java/com/jacr/HotelHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
